e74c1e4c24e6537dfaccfc974fbb84ec24db3ac0
Jan (LernMoment.de) <jan@lernmoment.de>
2018-03-28 02:30:02 +0200

    Shows Version of MediaInfo in x64

--- MediaInfoConsole/Module1.vb
+++ MediaInfoConsole/Module1.vb
@@ -1,7 +1,12 @@
 ﻿Module Module1
 
     Sub Main()
+        Using info As MediaInfo.DotNetWrapper.MediaInfo = New MediaInfo.DotNetWrapper.MediaInfo()
+            Console.WriteLine(info.Option("Info_Version"))
 
+            Console.WriteLine("Press Enter to quit!")
+            Console.ReadLine()
+        End Using
     End Sub
 
 End Module
